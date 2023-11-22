1e9b669c674cb7d283fe22c695e699652b52e563
yachaechae <wpql1207@gmail.com>
2023-11-22 16:50:04 +0900

    [ADD] 마이페이지 내에 이름 추가

--- src/components/MyapgeHeader.jsx
+++ src/components/MyapgeHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChangeImg, Image, MypageHeader } from '../style/MypageStyles';
+import { ChangeImg, Image, Info, MypageHeader } from '../style/MypageStyles';
 
 export default function MyapgeHeader() {
     return (
@@ -10,10 +10,9 @@ export default function MyapgeHeader() {
                         src="https://lh3.googleusercontent.com/a/ACg8ocLaP80xCrAJBkTaG3InkSmlzAL1-bAI-pdVIzWTLBOzuUs=s288-c-no"
                         alt=""
                     />
-                    <ChangeImg>
-                        <input type="file" value="이미지변경" />
-                    </ChangeImg>
+                    <ChangeImg>{/* <input type="file" value="이미지변경" /> */}</ChangeImg>
                 </Image>
+                <Info>이름</Info>
             </MypageHeader>
         </>
     );
--- src/style/MypageStyles.js
+++ src/style/MypageStyles.js
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 
 export const MypageHeader = styled.section`
     background: #eaeaea;
+    display: flex;
+    width: 100%;
+    gap: 1rem;
 `;
 export const Image = styled.div`
     background: #121212;
+    & img {
+        border-radius: 50%;
+    }
 `;
 export const ChangeImg = styled.div`
     background: #fff;
+`;
+export const Info = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
 `;
